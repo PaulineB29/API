bc96a2cca51d96f6b05e3e549bc9b35dbeca1f4e
PaulineB29 <104891500+PaulineB29@users.noreply.github.com>
2025-11-05 14:43:40 +0100

    Update analyses.js

--- backend/routes/analyses.js
+++ backend/routes/analyses.js
@@ -53,23 +53,41 @@ router.post('/', async (req, res) => {
     }
 
     // 2. Sauvegarder l'analyse
-    const analyseResult = await query(
-      `INSERT INTO analyses_buffett (
-        entreprise_id, date_analyse, periode, roe, marge_nette, marge_brute, 
-        marge_sga, roic, dette_equity, current_ratio, couverture_interets,
-        pe_ratio, earnings_yield, price_fcf, prix_vs_mm200, rendement_dividende,
-        pb_ratio, peg_ratio, ev_ebitda, score_global, recommandation,
-        points_forts, points_faibles
-      ) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, 
-                $15, $16, $17, $18, $19, $20, $21, $22, $23) RETURNING id`,
-      [
-        entrepriseId, date_analyse, periode, roe, marge_nette, marge_brute,
-        marge_sga, roic, dette_equity, current_ratio, couverture_interets,
-        pe_ratio, earnings_yield, price_fcf, prix_vs_mm200, rendement_dividende,
-        pb_ratio, peg_ratio, ev_ebitda, score_global, recommandation,
-        points_forts, points_faibles
-      ]
-    );
+const analyseResult = await query(
+    `INSERT INTO analyses_buffett (
+        entreprise_id, date_analyse, periode, 
+        // Métriques existantes
+        roe, marge_nette, marge_brute, marge_sga, roic,
+        dette_equity, current_ratio, couverture_interets,
+        pe_ratio, earnings_yield, price_fcf, prix_vs_mm200, 
+        rendement_dividende, pb_ratio, ev_ebitda,
+        score_global, recommandation, points_forts, points_faibles,
+        // NOUVELLES COLONNES
+        prix_actuel, mm_200, dividende_action, market_cap,
+        tresorerie, actifs_courants, passifs_courants, 
+        dette_totale, capitaux_propres, net_cash,
+        revenus, ebit, benefice_net, bpa, frais_financiers,
+        ebitda, cash_flow_operationnel, free_cash_flow
+    ) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, 
+              $15, $16, $17, $18, $19, $20, $21, $22, $23, $24, $25, $26,
+              $27, $28, $29, $30, $31, $32, $33, $34, $35, $36, $37, $38, $39) 
+    RETURNING id`,
+    [
+        entrepriseId, date_analyse, periode,
+        // Métriques existantes
+        roe, marge_nette, marge_brute, marge_sga, roic,
+        dette_equity, current_ratio, couverture_interets,
+        pe_ratio, earnings_yield, price_fcf, prix_vs_mm200,
+        rendement_dividende, pb_ratio, ev_ebitda,
+        score_global, recommandation, points_forts, points_faibles,
+        // Nouvelles colonnes
+        prix_actuel, mm_200, dividende_action, market_cap,
+        tresorerie, actifs_courants, passifs_courants,
+        dette_totale, capitaux_propres, net_cash,
+        revenus, ebit, benefice_net, bpa, frais_financiers,
+        ebitda, cash_flow_operationnel, free_cash_flow
+    ]
+);
 
     res.status(201).json({
       success: true,
